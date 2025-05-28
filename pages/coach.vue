b6494f1543d478cde3f2189b1347efd4dfe821fc
LKosters <lau.kosters@gmail.com>
2025-05-28 11:39:11 +0200

    Refactor routine pages for improved loading states, error handling, and layout consistency

--- pages/coach.vue
+++ pages/coach.vue
@@ -1,12 +1,11 @@
 <template>
-    <div>
-      <NuxtLink class="flex items-center gap-2 mb-5" to="#">
-        <Icon name="line-md:volume-low-filled" class="!size-5" />
-        Coming soon
-      </NuxtLink>
-      <BlockHero title="AI Coach" />
-    </div>
-  </template>
-  
-  <script lang="ts" setup></script>
-  
+  <div>
+    <NuxtLink class="flex items-center gap-2 mb-5" to="#">
+      <Icon name="line-md:volume-low-filled" class="!size-5" />
+      Coming soon
+    </NuxtLink>
+    <BlockHero title="AI Coach" />
+  </div>
+</template>
+
+<script lang="ts" setup></script>
